a73359b46028943c7f1570420d212aca839bb0c9
MacBook <macbook@MacBooks-MBP-2.lan>
2020-02-05 20:02:21 +0100

    t4-8 js completed

--- public/js/js_script.js
+++ public/js/js_script.js
@@ -22,12 +22,90 @@ let burger4 = new MenuItem("The Veggie Burger", "Bröd, 90g quornfärsburgare, s
 
 let burger5 = new MenuItem("The Future Burger", "Bröd, 90g halloumi, sallad, tomat, gurka", "gluten och laktose", "440", "https://www.burgerking.se/011_se/Product%20images/Veggie/image-thumb__7675__product_detail/HALLOUMI%20KING%201500x1500.png");
 
+
 console.log(nameAndKcal(burger1));
 console.log(nameAndKcal(burger2));
 console.log(nameAndKcal(burger3));
 console.log(nameAndKcal(burger4));
 console.log(nameAndKcal(burger5));
 
+let wrapper = document.getElementById('wrapper');
+let burgers = [burger1, burger2, burger3, burger4, burger5];
+let i = 0;
+for(i; i < burgers.length; i++)
+{
+    let burgerDiv = document.createElement('div');
+    let burgerImg = document.createElement('img');
+    let burgerPara = document.createElement('p');
+    let burgerName = document.createTextNode(burgers[i].name);
+    let burgerIng = document.createTextNode('Innehåll: ' + burgers[i].ingredients);
+    let br = document.createElement('br');
+    
+    burgerDiv.classList.add('box');
+    
+    burgerImg.src = burgers[i].stringPath;
+    burgerImg.height = 180;
+    burgerImg.width = 180;
+    
+    burgerPara.appendChild(burgerName);
+    
+    burgerDiv.appendChild(burgerPara);
+    burgerDiv.appendChild(burgerImg);
+    burgerDiv.appendChild(br);
+    burgerDiv.appendChild(burgerIng);
+    
+    if(burgers[i].allergy)
+        {
+            let br1 = document.createElement('br');
+            let burgerAllergy = document.createTextNode('allergi: ' + burgers[i].allergy);
+            burgerDiv.appendChild(br1);
+            burgerDiv.appendChild(burgerAllergy);
+        }
+
+    wrapper.appendChild(burgerDiv);
+}
+
+/*
+let div1 = document.createElement('div');
+let img1 = document.createElement('img');
+img1.height = 100;
+img1.width = 100;
+img1.src = burger1.stringPath;
+let txt1 = document.createTextNode('Hej hej');
+wrapper.appendChild(div1);
+div1.appendChild(txt1);
+div1.appendChild(img1);
+
+let div2 = document.createElement('div');
+let img2 = document.createElement('img');
+img2.src = burger2.stringPath;
+img2.height = 100;
+img2.width = 100;
+let txt2 = document.createTextNode('Hej hej');
+div2.appendChild(txt2);
+div2.appendChild(img2);
+wrapper.appendChild(div2);
+
+let div3 = document.createElement('div');
+let img3 = document.createElement('img');
+img3.src = burger3.stringPath;
+img3.height = 100;
+img3.width = 100;
+let txt3 = document.createTextNode('Hej hej');
+div3.appendChild(txt3);
+div3.appendChild(img3);
+wrapper.appendChild(div3);
+let div4 = document.createElement('div');
+let img4 = document.createElement('img');
+img4.src = burger4.stringPath;
+img4.height = 100;
+img4.width = 100;
+let txt4 = document.createTextNode('Hej hej');
+div4.appendChild(txt4);
+div4.appendChild(img4);
+wrapper.appendChild(div4);
+let div5 = document.createElement('div');
+*/
 //document.getElementById("myDiv").innerHTML = "Välj en burgare";
 /*
 function MenuItem(name, ingredients, allergy, kcal, stringPath) {
@@ -79,5 +157,25 @@ let p5 = document.createElement('p');
 let txt5 = document.createTextNode(nameAndKcal(burger5));
 p5.appendChild(txt5);
 document.body.appendChild(p5);
+
+let burgers = [burger1, burger2, burger3, burger4, burger5];
+
+let i = 0;
+let main = document.getElementById('myID');
+for (i; i < 5;i++)
+{
+	let currentBurger = burgers[i];
+  let allergy = "";
+	let p = document.createElement('p');
+  let txt3 = document.createTextNode(nameAndKcal(currentBurger));
+  if(currentBurger.allergy)
+  {
+  	allergy = currentBurger.allergy;
+    txt3 = document.createTextNode(nameAndKcal(currentBurger) + ' ' + allergy);
+  } 
+ 
+  p.appendChild(txt3);
+  main.appendChild(p);  
+}
 */
 
--- public/js/vue_script.js
+++ public/js/vue_script.js
@@ -16,5 +16,27 @@ const vm = new Vue({
     burger5: 'The Future Burger 440kcal',
   }
 })
+<div id="myID">
+    <h1>Välj en hamburgare</h1>
+    <p v-for="burger in burgers" v-if="burger.allergy">
+      {{ burger.name }} {{ burger.kcal}} kcal {{ burger.allergy }}
+    </p>    
+    <p v-else>
+      {{ burger.name }} {{ burger.kcal}} kcal
+    </p>
+</div>
 
+const vm = new Vue({
+  el: '#myID',
+  data: {
+  burgers: [
+  {name: 'The Void Burger', kcal: '60', allergy: 'gluten'}, 
+  {name: 'The Chees Burger', kcal: '450', allergy: 'gluten och laktos'},
+  {name: 'The Extra Meat-Burger', kcal: '640', allergy: 'gluten och laktos'},
+  {name: 'The Veggie Burger', kcal: '440', allergy: 'gluten'},
+  {name: 'The Future Burger', kcal: '440', allergy: 'gluten och laktos'},
+  ],
+    arbitraryVariableName: 'Välj en burgare ' + new Date(),
+  }
+})
 */
